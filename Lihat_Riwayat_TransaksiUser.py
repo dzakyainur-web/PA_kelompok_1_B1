a3484ab8a2a6a6f646ba3c5565ebbf5667780a25
larrynathanx <nathanaellarry@gmail.com>
2025-11-24 21:44:03 +0800

    larry nambahin dikit aja

--- Lihat_Riwayat_TransaksiUser.py
+++ Lihat_Riwayat_TransaksiUser.py
@@ -1,26 +1,51 @@
-from Data import users
+from Data import users, clear
+from prettytable import PrettyTable
 
-def Lihat_Riwayat_Transaksi_User():
+def Lihat_Riwayat_Transaksi_User(adminID):
     if len(users) == 0:
-        print("DATA USER BELUM ADA.")
+        print("TIDAK ADA USER TERDAFTAR.")
+        return
     
-    else:
-        print("===DATA USER===")
-        for key, value in users.items():
-            print(f"\n{key}\nNama: {value[0]}\nPassword: {value[1]}")
+    print("=== LIHAT RIWAYAT TRANSAKSI USER ===")
+    print(f"LOGIN SEBAGAI ADMIN: {adminID}\n")
+
+    user_list = list(users.items())
+    tabel_user = PrettyTable()
+    tabel_user.field_names(["User ID", "Username"])
+
+    for idx, (uid, data) in enumerate(user_list, start = 1):
+        tabel_user.add_row([idx, uid, data[0]])
+    print(tabel_user)
+    
+    try:
+        pilih = int(input("\nPilih nomor user yang ingin dilihat riwayatnya: "))
         
-        lihat = int(input("Lihat Riwayat Transaksi User Nomor Berapa? "))
-        daftar_key = list(users.keys())
-        key_dipilih = daftar_key[lihat - 1]
-        riwayat_transaksi = users[key_dipilih][2]
-        if len(riwayat_transaksi) == 0:
-            print(f"Tidak ada riwayat transaksi untuk user '{key_dipilih}'.")
+        if pilih < 1 or pilih > len(user_list):
+            raise ValueError("Nomor user tidak valid.")
+            
+        user_id, user_data = user_list[pilih - 1]
+
+        clear()
+        print(f"=== RIWAYAT TRANSAKSI {user_id} ===")
+        print(f"Nama: {user_data[0]}\n")
+        
+        riwayat = user_data[3]
+        
+        if not riwayat:
+            print("Riwayat transaksi kosong.")
+            
         else:
-            print(f"Riwayat Transaksi untuk user '{key_dipilih}':")
-            for transaksi in riwayat_transaksi:
-                print(f"{transaksi}")
-    input("\nTekan ENTER untuk kembali")
+            riwayat = PrettyTable()
+            riwayat.field_names = ["No", "Transaksi"]
+            for i, item in enumerate(riwayat, start = 1):
+                riwayat.add_row([i, item])
+            print(riwayat)
+
+    except ValueError as x:
+        print(f"Error: {x}")
+    input("\nTekan ENTER untuk kembali...")
 
 
-
+        
+        
     
