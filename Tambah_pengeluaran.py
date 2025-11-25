2f22c963d0baaa3c10461378d0b5ff8eb352bcd4
dzaky ainur <dzakyainur@MacBook-Pro-dzaky.local>
2025-11-25 23:29:35 +0800

    baru jek

--- Tambah_pengeluaran.py
+++ Tambah_pengeluaran.py
@@ -40,19 +40,10 @@ def Tambah_pengeluaran(userID):
 
     tanggal = datetime.now().strftime("%d-%m-%y")
     users[userID][2] -= jumlah
-<<<<<<< Updated upstream
-    users[userID][4].append([jumlah, untuk, tanggal])
-    table = PrettyTable(["Jumlah", "untuk", "Tanggal"])
-    table.add_row([jumlah, untuk, tanggal])
-    print("\Pengeluaran berhasil ditambahkan!\n")
-=======
     users[userID][4].append([jumlah, Tujuan, tanggal])
-        
-    table = PrettyTable(["Jumlah", "Tujuan", "Tanggal"])
+    table = PrettyTable(["Jumlah", "untuk", "Tanggal"])
     table.add_row([jumlah, Tujuan, tanggal])
-
-    print("\nPengeluaran berhasil ditambahkan!\n")
->>>>>>> Stashed changes
+    print("\Pengeluaran berhasil ditambahkan!\n")
     print(table)
     input("\nTekan ENTER untuk kembali...")
 
