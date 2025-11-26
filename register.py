d3ac3ed657072d753c2477e9a15517f4ac8f7dfc
dzaky ainur <dzakyainur@MacBook-Pro-dzaky.local>
2025-11-26 22:13:07 +0800

    jaki baru

--- register.py
+++ register.py
@@ -18,14 +18,9 @@ def regis():
             reg = int(input("REGISTRASI SEBAGAI APA? "))
 
             # ==================== REGISTRASI ADMIN ====================
+            
             if reg == 1:
-                clear()
-                print("======= SILAKAN REGISTRASI ADMIN BARU ========")
-                print("= KETIK 'BATAL' UNTUK MEMBATALKAN REGISTRASI =")
-                print("========= INPUT TIDAK BOLEH KOSONG ===========")
-
-                username = input("\nMasukkan username: ").strip()
-                while username == "":
+                while True:
                     clear()
                     print("======= SILAKAN REGISTRASI ADMIN BARU ========")
                     print("= KETIK 'BATAL' UNTUK MEMBATALKAN REGISTRASI =")
@@ -33,67 +28,74 @@ def regis():
 
                     username = input("\nMasukkan username: ").strip()
 
-                if username == "BATAL":
-                    clear()
-                    print("REGISTRASI DIBATALKAN")
-                    regis = False
-                    return
+                    if username == "":
+                        clear()
+                        print("\nINPUT TIDAK BOLEH KOSONG")
+                        input("\nTekan ENTER untuk kembali...")
+                        continue
 
-                password = input("Masukkan password: ").strip()
-                while password == "":
+                    if username.upper() == "BATAL":
+                        clear()
+                        print("REGISTRASI DIBATALKAN")
+                        input("\nTekan ENTER untuk kembali...")
+                        regis = False
+                        return
+
                     password = input("Masukkan password: ").strip()
+                    while password == "":
+                        password = input("Masukkan password: ").strip()
 
-                # Cek apakah username sudah digunakan
-                cek = any(admin[key][0] == username for key in admin)
+                    cek = any(admin[key][0] == username for key in admin)
 
-                if cek:
-                    print("===== USERNAME SUDAH TERPAKAI =====\n")
-                else:
-                    key_baru = f"admin_{len(admin) + 1}"
-                    admin[key_baru] = [username, password]
-                    print("== BERHASIL REGISTRASI ADMIN ==")
-                    input("Tekan ENTER untuk melanjutkan...")
-                    clear()
-                    regis = False
+                    if cek:
+                        print("===== USERNAME SUDAH TERPAKAI =====\n")
+                    else:
+                        key_baru = f"admin_{len(admin) + 1}"
+                        admin[key_baru] = [username, password]
+                        print("== BERHASIL REGISTRASI ADMIN ==")
+                        input("Tekan ENTER untuk melanjutkan...")
+                        clear()
+                        regis = False
 
             # ==================== REGISTRASI USER ====================
             elif reg == 2:
-                clear()
-                print("======== SILAKAN REGISTRASI USER BARU ========")
-                print("= KETIK 'BATAL' UNTUK MEMBATALKAN REGISTRASI =")
-                print("========= INPUT TIDAK BOLEH KOSONG ===========")
-
-
-                username = input("\nMasukkan username: ").strip()
-                while username == "":
+                while True:
                     clear()
                     print("======== SILAKAN REGISTRASI USER BARU ========")
                     print("= KETIK 'BATAL' UNTUK MEMBATALKAN REGISTRASI =")
                     print("========= INPUT TIDAK BOLEH KOSONG ===========")
+
+
                     username = input("\nMasukkan username: ").strip()
+                    if username == "":
+                        clear()
+                        print("\nINPUT TIDAK BOLEH KOSONG")
+                        input("\nTekan ENTER untuk kembali...")
+                        continue
 
-                if username == "BATAL":
-                    clear()
-                    print("REGISTRASI DIBATALKAN")
-                    regis = False
-                    return
-                
-                password = input("Masukkan password: ").strip()
-                while password == "":
+                    if username.upper() == "BATAL":
+                        clear()
+                        print("REGISTRASI DIBATALKAN")
+                        input("\nTekan ENTER untuk kembali...")
+                        regis = False
+                        return
+                    
                     password = input("Masukkan password: ").strip()
+                    while password == "":
+                        password = input("Masukkan password: ").strip()
 
-                # Cek apakah username sudah digunakan
-                cek = any(users[key][0] == username for key in users)
+                    # Cek apakah username sudah digunakan
+                    cek = any(users[key][0] == username for key in users)
 
-                if cek:
-                    print("===== USERNAME SUDAH TERPAKAI =====\n")
-                else:
-                    key_baru = f"user_{len(users) + 1}"
-                    users[key_baru] = [username, password, 0, [], []]
-                    print("== BERHASIL REGISTRASI USER ==")
-                    input("Tekan ENTER untuk melanjutkan...")
-                    clear()
-                    regis = False
+                    if cek:
+                        print("===== USERNAME SUDAH TERPAKAI =====\n")
+                    else:
+                        key_baru = f"user_{len(users) + 1}"
+                        users[key_baru] = [username, password, 0, [], []]
+                        print("== BERHASIL REGISTRASI USER ==")
+                        input("Tekan ENTER untuk melanjutkan...")
+                        clear()
+                        regis = False
 
             # ==================== KELUAR ====================
             elif reg == 3:
